563adee0802db6ed2fb253d34b156efdee3515f6
ShellBin <maoxiaobing@live.com>
2022-08-25 17:00:34 +0800

    update

--- plugin/fluent_emoji.php
+++ plugin/fluent_emoji.php
@@ -3,7 +3,7 @@
 Plugin Name: Fluent Emoji for WordPress
 Plugin URI: https://blog.shellbin.me/?p=2208
 Description: Switch your WordPress site from Twemoji to Fluent Emoji.
-Version: 1.0
+Version: 0.1
 Author: ShellBin
 Author URI: https://shellbin.me
 */
@@ -11,7 +11,11 @@ Author URI: https://shellbin.me
 add_filter( 'emoji_svg_url', 'replace_SVG' );
 add_filter( 'emoji_url', 'replace_PNG' );
 
-function emoji_url_typeSVG( $url ) {
+function replace_SVG( $url ) {
+    return 'https://blog.shellbin.me/wp-content/uploads/theme-resource/emoji/';
+}
+
+function replace_PNG( $url ) {
     return 'https://blog.shellbin.me/wp-content/uploads/theme-resource/emoji/';
 }
 ?>
